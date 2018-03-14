ae50a6411dcb835dd018ebecefe329dda5dd0547
Ugnius <ugnius.malukas@gigatronik.com>
2018-03-14 08:39:57 +0100

    Added app data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <QApplication>
+#include <QSettings>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
+
+    // Settings.
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    QApplication::setOrganizationName("instant_translator");
+    QApplication::setApplicationName("instant_translator");
+
     MainWindow mainWindow;
     mainWindow.show();
 
--- src/utils/googleapi.cpp
+++ src/utils/googleapi.cpp
@@ -1,6 +1,11 @@
+#include <QSettings>
+#include <QDebug>
 #include "googleapi.h"
 
 GoogleAPI::GoogleAPI() {
+    QSettings settings;
+    QString key = settings.value("api/google/key").toString();
+    qDebug() << key;
 }
 
 GoogleAPI::~GoogleAPI() {
