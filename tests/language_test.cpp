4c25866f554042001858cfd1b3529da3e01c2fd5
Ugnius <ugnius.malukas@gmail.com>
2018-04-24 22:15:36 +0200

    Covered Language class.

--- tests/language_test.cpp
+++ tests/language_test.cpp
@@ -1,8 +1,35 @@
 #include <gtest/gtest.h>
+#include <QStringList>
 #include "utils/language.h"
 
-TEST(Language, getCode)
-{
+TEST(Language, getCode) {
     Language language;
     ASSERT_EQ("lt", language.getCode("Lithuanian"));
+}
+
+TEST(Language, getName) {
+    Language language;
+    ASSERT_EQ("Lithuanian", language.getName("lt"));
+}
+
+TEST(Language, getLanguages) {
+    Language language;
+    QStringList codesList;
+    codesList.append("lt");
+    codesList.append("en");
+    QStringList languagesList = language.getLanguages(codesList);
+    ASSERT_EQ(2, languagesList.size());
+    ASSERT_EQ("Lithuanian", languagesList.at(0));
+    ASSERT_EQ("English", languagesList.at(1));
+}
+
+TEST(Language, getCodes) {
+    Language language;
+    QStringList languagesList;
+    languagesList.append("Lithuanian");
+    languagesList.append("English");
+    QStringList codesList = language.getCodes(languagesList);
+    ASSERT_EQ(2, languagesList.size());
+    ASSERT_EQ("lt", codesList.at(0));
+    ASSERT_EQ("en", codesList.at(1));
 }
