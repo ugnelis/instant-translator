4c43f3c3b11130266e58d50a8ec8b392f7572d28
Ugnius <ugnius.malukas@gmail.com>
2018-05-02 22:16:00 +0200

    Added RequestManager test.

--- tests/utils/requestmanager_test.cpp
+++ tests/utils/requestmanager_test.cpp
@@ -9,7 +9,7 @@
 
 class MockQNetworkAccessManager : public QNetworkAccessManager {
 public:
-    MockQNetworkAccessManager(QObject *parent = nullptr) : QNetworkAccessManager(parent) {}
+    explicit MockQNetworkAccessManager(QObject *parent = nullptr) : QNetworkAccessManager(parent) {}
 
 protected:
     QNetworkReply *createRequest(Operation op, const QNetworkRequest &originalReq,
@@ -49,6 +49,36 @@ TEST(RequestManagerTests, postRequest_SendPostRequest_True) {
     requestManager.postRequest(request, postJsonDocument.toJson());
 
     // Test.
-    QString expexted = "{\"data\":{\"translations\":[{\"translatedText\":\"Hello\"}]}}";
-    ASSERT_EQ(expexted, requestManager.getReply());
+    QString expected = "{\"data\":{\"translations\":[{\"translatedText\":\"Hello\"}]}}";
+    ASSERT_EQ(expected, requestManager.getReply());
+}
+
+TEST(RequestManagerTests, getRequest_SendGetRequest_True) {
+    int argc = 0;
+    char **argv = 0;
+    QCoreApplication app(argc, argv);
+
+    // Format Json object.
+    QJsonObject postJsonObject;
+    postJsonObject["source"] = "lt";
+    postJsonObject["target"] = "en";
+    postJsonObject["format"] = "text";
+    postJsonObject["q"] = "Labas";
+
+    QJsonDocument postJsonDocument(postJsonObject);
+
+    // Make request.
+    QUrl url("https://randomurl.com/");
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // Send request.
+    MockQNetworkAccessManager *manager = new MockQNetworkAccessManager();
+
+    RequestManager requestManager(nullptr, manager);
+    requestManager.getRequest(request);
+
+    // Test.
+    QString expected = "{\"data\":{\"translations\":[{\"translatedText\":\"Hello\"}]}}";
+    ASSERT_EQ(expected, requestManager.getReply());
 }
